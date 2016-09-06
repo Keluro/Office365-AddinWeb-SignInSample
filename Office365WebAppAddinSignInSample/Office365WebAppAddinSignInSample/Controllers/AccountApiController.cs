55481a66a4150c2323dbfc6b2118d623a1109ad3
Benoit Patra <bpatra@keluro.com>
2016-09-06 21:04:13 +0200

    Remove unused dependencies and add the AccountApiController

---
+++ Office365WebAppAddinSignInSample/Office365WebAppAddinSignInSample/Controllers/AccountApiController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+
+namespace Office365WebAppAddinSignInSample.Controllers
+{
+    [Authorize]
+    public class AccountApiController : ApiController
+    {
+
+        public AccountApiController()
+        {
+        }
+
+        [HttpGet]
+        public async Task<string> CurrentUser()
+        {
+            //var info = await _connectedUserProvider.GetMailUserInfo(_office365WebCalls);
+            //return new MailUserInfoDto()
+            //{
+            //    ObjectId = info.UserObjectId,
+            //    DisplayName = info.DisplayName,
+            //    UpnName = info.UpnName,
+            //    Email = info.Email,
+            //    ThumbnailUri = MicrosoftRestCallsManager.GetThumbnailClientUrl(info.UpnName)
+            //};
+            return "Mr hardcoded";
+        }
+    }
+}
